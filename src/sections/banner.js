088606095233ef36eb9828e5598fbcbc182765fb
Samrath Chauhan <samrathchauhan49@gmail.com>
2021-10-10 17:05:55 +0530

    Banner done

--- src/sections/banner.js
+++ src/sections/banner.js
@@ -7,7 +7,22 @@ import ShapeRight from 'assets/shape-right.png';
 
 export default function Banner() {
   return (
-    <h1>Banner</h1>
+    <section sx={styles.banner} id="home">
+      <Container sx={styles.banner.container}>
+        <Box sx={styles.banner.contentBox}>
+          <Heading as="h1" variant="heroPrimary">
+            Top Quality Digital Product to Explore
+          </Heading>
+          <Text as="p" variant="heroSecondary">
+            Check out our website to find great software products and deals 🔥.
+          </Text>
+          <Button variant="primary">Explore</Button>
+        </Box>
+        <Box sx={styles.banner.imageBox}>
+          <Image src={BannerImg} alt="Banner" />
+        </Box>
+      </Container>
+    </section>
   );
 }
 
