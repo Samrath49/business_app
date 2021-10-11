957124e2ea5c8b41acb65fc3e2263f2b9751b33e
Samrath Chauhan <samrathchauhan49@gmail.com>
2021-10-11 23:28:36 +0530

    Text feature done

--- src/components/text-feature.js
+++ src/components/text-feature.js
@@ -22,6 +22,14 @@ export default function TextFeature({
           {description}
         </Text>
       )}
+
+      {btnName && (
+        <Link href={btnURL} variant="default">
+          <Button variant="primary" aria-label={btnName}>
+            {btnName}
+          </Button>
+        </Link>
+      )}
     </Box>
   );
 }
