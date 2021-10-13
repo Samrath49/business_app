3835a58d83741a4254bcf8da2461be2054208e94
Samrath Chauhan <samrathchauhan49@gmail.com>
2021-10-13 13:15:01 +0530

    Workflow section done

--- src/sections/workflow.js
+++ src/sections/workflow.js
@@ -47,6 +47,10 @@ export default function WorkFlow() {
           {data.map((item) => (
             <Box sx={styles.card} key={item.id}>
               <Box sx={styles.iconBox}>{`0${item.id}`}</Box>
+              <Box sx={styles.wrapper}>
+                <Heading sx={styles.wrapper.title}>{item.title}</Heading>
+                <Text sx={styles.wrapper.subTitle}>{item.text}</Text>
+              </Box>
             </Box>
           ))}
         </Grid>
