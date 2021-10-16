ed4cc2cb641a44e7717e746a969e2fe56bfecbbd
Samrath Chauhan <samrathchauhan49@gmail.com>
2021-10-16 16:04:49 +0530

    Footer done

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -8,7 +8,18 @@ export default function Footer() {
   return (
     <footer sx={styles.footer}>
       <Container>
-        <Box sx={styles.footer.footerBottomArea}></Box>
+        <Box sx={styles.footer.footerBottomArea}>
+          <Link path="/">
+            <Image src={FooterLogo} alt="Logo" />
+          </Link>
+          <Box sx={styles.footer.menus}>
+            <nav>
+              {data.menuItem.map((item, i) => (
+                <Link path={item.path} key={i} label={item.label} sx={styles.footer.link} />
+              ))}
+            </nav>
+          </Box>
+        </Box>
       </Container>
     </footer>
   );
